cdecdf0e0f145fca177c77e5bb5b0f93db22b033
Sudharshan <sudhar393@gmail.com>
2018-03-06 12:20:20 +0800

    added arg processing

--- fcc.c
+++ fcc.c
@@ -8,14 +8,30 @@
 #include "string.h"
 
 // Global Variable declarations
-
+FILE *asm_fp;
 // Private Function prototypes
 
 // Main Loop
 int main(int argc, const char* argv[]) {
+    // Local Variable declaration
+    char* file_location;
+
     // Basic argument parsing and error checking
     if(argc < 2) {
         printf("Invalid number of args passed : At least 1 argument required");
         return 0;
     }
+
+    strcpy(file_location, argv[1]); // Assign second variable to be asm file location
+
+    asm_fp = fopen(file_location, "r");
+    // Check if file_location is valid
+    if(asm_fp == NULL) {
+        printf("No such file found");
+        return 0;
+    }
+    else
+        printf("%s", file_location);
+
+    return 0;
 }
