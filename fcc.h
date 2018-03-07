93a330ba58b186ce3218bd9fb3a04a35d4e25701
Sudharshan <sudhar393@gmail.com>
2018-03-08 07:23:13 +0800

    added opcode table

--- fcc.h
+++ fcc.h
@@ -1,4 +1,26 @@
 #ifndef FCC_H_
 #define FCC_H_
 
+#include "uthash.h"
+
+struct dict_struct {
+    char mnemonic[3];
+    int opcode;
+    UT_hash_handle hh;
+}
+
+const char* opcode[] = {"add",
+                        "sub",
+                        "orr",
+                        "and",
+                        "xor",
+                        "not",
+                        "rdr",
+                        "wrr",
+                        "ldr",
+                        "cmp",
+                        "shl",
+                        "shr",
+                        "jmp",
+                        "jmpeq"}
 #endif /* FCC_H_ */
